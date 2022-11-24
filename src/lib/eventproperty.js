5d47a0fc3380f480a7d92e805de479d049f2e8a5
pony <luckyliangfei@gmail.com>
2022-11-24 15:03:08 +0800

    升级版本到3.2.5

--- src/lib/eventproperty.js
+++ src/lib/eventproperty.js
@@ -40,7 +40,7 @@ export const eventProperty = {
         $model: info.model,
         $brand: String(info.brand).toLocaleUpperCase(),
         $manufacturer: info.brand,
-        $lib_version: "1.0.0",
+        $lib_version: "3.2.5",
       };
       for (let item in temp_systeminfo) {
         if (temp_systeminfo.hasOwnProperty(item)) {
